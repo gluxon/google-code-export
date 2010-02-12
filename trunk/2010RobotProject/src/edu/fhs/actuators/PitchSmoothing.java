a52434fbd9dd13959aa3ca57c32c15342495ec56
cunninghampatt <cunninghampatt@29b1f36c-e104-11dd-a550-c997509ed985>
2010-02-12 14:51:39 +0000

    Added comments and credits for everyone who worked on the Pitch Speed Smoother

--- trunk/2010RobotProject/src/edu/fhs/actuators/PitchSmoothing.java
+++ trunk/2010RobotProject/src/edu/fhs/actuators/PitchSmoothing.java
@@ -1,21 +1,15 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package edu.fhs.actuators;
 
 /**
- *
- * @author Patt
+ * @author Patt Cunningham / Adam Gresh / Brandon Bald / Jim Jakupco / Wess Stevens
  */
 public class PitchSmoothing
 {
-    private final int smoothingFactor;
-    private static final double ANG_CUTOFF = 45.0;
+    private final int smoothingFactor;//Initilizes the smoothing factor variable
+    private static final double ANG_CUTOFF = 45.0; //Sets the maximum angle or the ANG_CUTOFF to 45 degrees
 
     public PitchSmoothing(int i) {
-        this.smoothingFactor = i;
+        this.smoothingFactor = i; //Creates the smoothing factor in pitch / (ANG_CUTOFF * smoothingFactor)
     }
     public double gyroAutonomousAngleSpeedAdjust(double motorInput, boolean autonomous, double pitch)
     {
@@ -27,7 +21,7 @@ public class PitchSmoothing
                 if(pitch / ANG_CUTOFF >= 1) //If the angle of the robot gets to big it will automaticaly cut of the motors
                 {
                         System.out.println("Angle of robot has passed the angle cutoff. The robot will now stop.");//Sends a message to console that tells the user that the angle limit has been reached or exceeded
-                        newSpeed = 0.0;
+                        newSpeed = 0.0;//Output's 0.0 for the motor if the angle cutoff gets exceeded
                 }
                 else//If the input is not reached or excedded the angCutoff limiter
                 {
@@ -37,9 +31,9 @@ public class PitchSmoothing
         }
         else
         {
-        System.out.println("The robot is not in autonomous mode!");
-        newSpeed = 0.0;
+        System.out.println("The robot is not in autonomous mode!");//Prints out this into console if the robot is not in autonomous mode when executing the method
+        newSpeed = 0.0;//Output's motor to 0.0 if the robot is not in autonomous mode
         }
-        return newSpeed;
+        return newSpeed;//Return's the new modified speed
     }
 }
