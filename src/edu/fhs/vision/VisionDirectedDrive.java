7c40a6a64c5befd219dadb8dda54a6adaaa009b6
stephenmcginty@comcast.net <stephenmcginty@comcast.net@29b1f36c-e104-11dd-a550-c997509ed985>
2010-02-12 23:51:53 +0000

    Update to VisionDirected Drive.  Initial Autonomous code.

--- src/edu/fhs/vision/VisionDirectedDrive.java
+++ src/edu/fhs/vision/VisionDirectedDrive.java
@@ -11,128 +11,64 @@ import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.PIDController;
 import edu.wpi.first.wpilibj.PIDOutput;
 import edu.wpi.first.wpilibj.RobotDrive;
-import edu.wpi.first.wpilibj.camera.AxisCamera;
-import edu.wpi.first.wpilibj.camera.AxisCameraException;
-import edu.wpi.first.wpilibj.image.ColorImage;
-import edu.wpi.first.wpilibj.image.NIVisionException;
-import edu.wpi.first.wpilibj.samples.Target;
+import edu.wpi.first.wpilibj.Ultrasonic;
 
 /**
  *
  * @author programming
  */
-public class VisionDirectedDrive {
+public class VisionDirectedDrive{
 
     private Gyro gyro;
     private Joystick js;
-    private PIDController turnController;
     private RobotDrive drive;
-    private AxisCamera cam;
-    private boolean lastEnable;
-    private final double kScoreThreshold = .01;
-    private boolean targetFound;
+    private CircleFinder circleFinder;
+    private int mode;
+    private int lastMode;
+    private final double MINUMUM_RANGE = 10.0;
+    private PIDController driveTowardsRamp;
+    private Ultrasonic ultraFrontRight;
 
-
-
-    public VisionDirectedDrive(Gyro g , Joystick j, RobotDrive d){
-        lastEnable = false;
+    public VisionDirectedDrive(Gyro g, Joystick j, RobotDrive d,Ultrasonic fr){
         gyro = g;
         js = j;
         drive = d;
-        cam = AxisCamera.getInstance();
-        turnController = new PIDController(.08 , 0.0 , 0.5 , gyro , new PIDOutput(){
-            public void pidWrite(double output) {
-                drive.holonomicDrive(js.getMagnitude(), js.getDirectionDegrees(), output);
+        circleFinder = new CircleFinder(g,j,d);
+        mode = 0;
+        lastMode = 0;
+        ultraFrontRight = fr;
+        driveTowardsRamp = new PIDController(.5,.2,.8,ultraFrontRight,new PIDOutput(){
+            public void pidWrite(double output){
+                drive.holonomicDrive(output, 0, 0);
             }
-        }, .005);
-        targetFound = false;
+        });
     }
-
-    public void intialize(){ //call during robotInit
-        cam.writeResolution(AxisCamera.ResolutionT.k320x240);
-        cam.writeBrightness(0);
-        gyro.setSensitivity(.007);
-        turnController.setInputRange(-360.0, 360.0);
-        turnController.setTolerance(1 / 90. * 100);
-        turnController.disable();
+    
+    public void initialize(){
+        circleFinder.intialize();
+        driveTowardsRamp.setTolerance(5.0);
+        driveTowardsRamp.setSetpoint(MINUMUM_RANGE);
     }
+    
+    public void autonomousZoneOne(){
 
-    public void centerOnCircle(boolean enable){ //enables PID controller.  Will overide user input to robot.
-        if(enable == false){
-            turnController.disable();
-            lastEnable = false;
-        }else{
-            if(!lastEnable){
-                lastEnable = true;
-                turnController.enable();
-                turnController.setSetpoint(gyro.pidGet());
-            }
-            ColorImage image = null;
-            try {
-                if (cam.freshImage()) {// && turnController.onTarget()) {
-                    targetFound = false;
-                    double gyroAngle = gyro.pidGet();
-                    image = cam.getImage();
-                    Thread.yield();
-                    Target[] targets = Target.findCircularTargets(image);
-                    Thread.yield();
-                    if (targets.length == 0 || targets[0].m_score < kScoreThreshold) {
-                        System.out.println("No target found");
-                        Target[] newTargets = new Target[targets.length + 1];
-                        newTargets[0] = new Target();
-                        newTargets[0].m_majorRadius = 0;
-                        newTargets[0].m_minorRadius = 0;
-                        newTargets[0].m_score = 0;
-                        for (int i = 0; i < targets.length; i++) {
-                            newTargets[i + 1] = targets[i];
-                        }
-                        //noCircleFound();
-                    } else {
-                        targetFound = true;
-                        System.out.println(targets[0]);
-                        System.out.println("Target Angle: " + targets[0].getHorizontalAngle());
-                        turnController.setSetpoint(gyroAngle + targets[0].getHorizontalAngle());
-                    }
-                }
-            } catch (NIVisionException ex) {
-                ex.printStackTrace();
-            } catch (AxisCameraException ex) {
-                ex.printStackTrace();
-            } finally {
-                try {
-                    if (image != null) {
-                        image.free();
-                    }
-                } catch (NIVisionException ex) {
-                }
-            }
+        
+        switch (mode){
+            case 0: circleFinder.centerOnCircle(!circleFinder.isOnTarget());
+                    if(circleFinder.isOnTarget()){
+                        mode++;
+                    }break;
+            case 1: if(lastMode ==0){
+                        driveTowardsRamp.enable();
+                        lastMode++;
+                    }else if(driveTowardsRamp.onTarget()){
+                        driveTowardsRamp.disable();
+                        mode++;
+                    }break;
+            default: break;
         }
+        
     }
-
-
-    private void noCircleFound(){
-        turnController.disable();
-        drive.holonomicDrive(0, 0, -.2);
-        turnController.enable();
-    }
-
-    public boolean isTargetFound(){
-        return targetFound;
-    }
-
-    public boolean isOnTarget(){
-        if(targetFound == true)
-            return turnController.onTarget();
-        return false;
-    }
-
-    public void driveToRamp(){
-        if (isOnTarget() == false){
-            centerOnCircle(true);
-        }else{
-            centerOnCircle(false);
-            drive.holonomicDrive(.5, 0, 0);
-        }
-    }
-
 }
+
+   
