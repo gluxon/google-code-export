e773da7d3242b6ce0c5a55bdf5723072a1cc047a
adamgresh@gmail.com <adamgresh@gmail.com@29b1f36c-e104-11dd-a550-c997509ed985>
2010-02-12 00:50:34 +0000

    smoothing code for going over bumps

--- trunk/2010RobotProject/src/edu/fhs/actuators/PitchSmoothing.java
+++ trunk/2010RobotProject/src/edu/fhs/actuators/PitchSmoothing.java
@@ -12,25 +12,26 @@ package edu.fhs.actuators;
 public class PitchSmoothing
 {
     private final int smoothingFactor;
+    private static final double ANG_CUTOFF = 45.0;
 
     public PitchSmoothing(int i) {
         this.smoothingFactor = i;
     }
-    public double gyroAutonomousAngleSpeedAdjust(double angCutoff, double motorInput, boolean autonomous, double pitch)
+    public double gyroAutonomousAngleSpeedAdjust(double motorInput, boolean autonomous, double pitch)
     {
         double newSpeed = 0.0d;//The value to return
         if(autonomous)//This loops the code as long as the robot is enabled
         {
             if(motorInput < -0.2 && motorInput > 0.2)//This makes sure that the robot doesnt move when the input is 0.1 0.0 or -0.1
             {
-                if(pitch / angCutoff >= 1) //If the angle of the robot gets to big it will automaticaly cut of the motors
+                if(pitch / ANG_CUTOFF >= 1) //If the angle of the robot gets to big it will automaticaly cut of the motors
                 {
                         System.out.println("Angle of robot has passed the angle cutoff. The robot will now stop.");//Sends a message to console that tells the user that the angle limit has been reached or exceeded
                         newSpeed = 0.0;
                 }
                 else//If the input is not reached or excedded the angCutoff limiter
                 {
-                    newSpeed = motorInput - pitch / (angCutoff * smoothingFactor);//Automatically adjust's the speed based on the angle
+                    newSpeed = motorInput - pitch / (ANG_CUTOFF * smoothingFactor);//Automatically adjust's the speed based on the angle
                 }
             }
         }
