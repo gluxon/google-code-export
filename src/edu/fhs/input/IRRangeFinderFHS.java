b6168732b6144efabb5e30339bf7d45c8868f756
stephenmcginty@comcast.net <stephenmcginty@comcast.net@29b1f36c-e104-11dd-a550-c997509ed985>
2010-02-18 22:08:07 +0000

    forgot to add classes

---
+++ src/edu/fhs/input/IRRangeFinderFHS.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package edu.fhs.input;
+
+/**
+ *
+ * @author programming
+ */
+public class IRRangeFinderFHS {
+
+}
