30ed9532587e5250c5670a335f9b136d131e1259
adamgresh <adamgresh@29b1f36c-e104-11dd-a550-c997509ed985>
2010-06-01 21:27:45 +0000

    take out code for candy cannon and format circle finder

--- trunk/2010RobotProject/src/edu/fhs/vision/CircleFinder.java
+++ trunk/2010RobotProject/src/edu/fhs/vision/CircleFinder.java
@@ -2,7 +2,6 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package edu.fhs.vision;
 
 import edu.fhs.util.StringUtil;
@@ -23,182 +22,174 @@ import edu.wpi.first.wpilibj.samples.Target;
  *
  * @author programming
  */
-public class CircleFinder{
+public class CircleFinder {
 
-    private Gyro gyro;
-    private Joystick js;
-    private PIDController turnController;
-    private RobotDrive drive;
-    private AxisCamera cam;
-    private boolean lastEnable;
-    private final double kScoreThreshold = .01;
-    private boolean targetFound;
+	private Gyro gyro;
+	private Joystick js;
+	private PIDController turnController;
+	private RobotDrive drive;
+	private AxisCamera cam;
+	private boolean lastEnable;
+	private final double kScoreThreshold = .01;
+	private boolean targetFound;
 
-
-
-
-    public CircleFinder(Gyro Gyro , Joystick joystick, RobotDrive robotDrive){
-        lastEnable = false;
-        gyro = Gyro;
-        js = joystick;
-        drive = robotDrive;
-        cam = AxisCamera.getInstance();
+	public CircleFinder(Gyro Gyro, Joystick joystick, RobotDrive robotDrive) {
+		lastEnable = false;
+		gyro = Gyro;
+		js = joystick;
+		drive = robotDrive;
+		cam = AxisCamera.getInstance();
 		cam.writeResolution(AxisCamera.ResolutionT.k320x240);
-        cam.writeBrightness(0);
-        gyro.setSensitivity(.007);
+		cam.writeBrightness(0);
+		gyro.setSensitivity(.007);
 
-        turnController = new PIDController(.08 , 0.0 , 0.5 , gyro , new PIDOutput(){
-            public void pidWrite(double output) {
-                drive.holonomicDrive(js.getMagnitude(), js.getDirectionDegrees(), output);
-            }
-        }, .005);
+		turnController = new PIDController(.08, 0.0, 0.5, gyro, new PIDOutput() {
+
+			public void pidWrite(double output) {
+				drive.holonomicDrive(js.getMagnitude(), js.getDirectionDegrees(), output);
+			}
+		}, .005);
 		turnController.setInputRange(-360.0, 360.0);
-        turnController.setTolerance(1 / 90. * 100);
-        turnController.disable();
-        targetFound = false;
-    }
+		turnController.setTolerance(1 / 90. * 100);
+		turnController.disable();
+		targetFound = false;
+	}
 
-    public void intialize(){ //call during robotInit
-        cam.writeResolution(AxisCamera.ResolutionT.k320x240);
-        cam.writeBrightness(0);
-        gyro.setSensitivity(.007);
-        turnController.setInputRange(-360.0, 360.0);
-        turnController.setTolerance(1 / 90. * 100);
-        turnController.disable();
-    }
+	public void intialize() { //call during robotInit
+		cam.writeResolution(AxisCamera.ResolutionT.k320x240);
+		cam.writeBrightness(0);
+		gyro.setSensitivity(.007);
+		turnController.setInputRange(-360.0, 360.0);
+		turnController.setTolerance(1 / 90. * 100);
+		turnController.disable();
+	}
 
-    
+	public void centerOnCircle(boolean enable) { //enables PID controller.  Will overide user input to robot.
+		if (enable == false) {
+			DriverStationLCD.getInstance().println(DriverStationLCD.Line.kUser5, 1, "TARGET DISABLED");
+			turnController.disable();
+			lastEnable = false;
+		} else {
+			DriverStationLCD.getInstance().println(DriverStationLCD.Line.kUser5, 1, "TARGET ENABLED   ");
+			if (!lastEnable) {
+				lastEnable = true;
+				turnController.enable();
+				turnController.setSetpoint(gyro.pidGet());
+			}
+			lastEnable = true;
+			ColorImage image = null;
+			try {
+				if (cam.freshImage()) {// && turnController.onTarget()) {
+					double gyroAngle = gyro.pidGet();
+					image = cam.getImage();
+					Thread.yield();
+					Target[] targets = Target.findCircularTargets(image);
+					Thread.yield();
+					if (targets.length == 0 || targets[0].m_score < kScoreThreshold) {
+						System.out.println("No target found");
+						Target[] newTargets = new Target[targets.length + 1];
+						newTargets[0] = new Target();
+						newTargets[0].m_majorRadius = 0;
+						newTargets[0].m_minorRadius = 0;
+						newTargets[0].m_score = 0;
+						for (int i = 0; i < targets.length; i++) {
+							newTargets[i + 1] = targets[i];
+						}
+						//trackerDashboard.updateVisionDashboard(0.0, gyro.getAngle(), 0.0, 0.0, newTargets);
+					} else {
+						System.out.println(targets[0]);
+						System.out.println("Target Angle: " + targets[0].getHorizontalAngle());
+						turnController.setSetpoint(gyroAngle + targets[0].getHorizontalAngle());
+						//trackerDashboard.updateVisionDashboard(0.0, gyro.getAngle(), 0.0, targets[0].m_xPos / targets[0].m_xMax, targets);
+					}
+				}
+			} catch (NIVisionException ex) {
+				ex.printStackTrace();
+			} catch (AxisCameraException ex) {
+				ex.printStackTrace();
+			} finally {
+				try {
+					if (image != null) {
+						image.free();
+					}
+				} catch (NIVisionException ex) {
+				}
+			}
+		}
+	}
 
-    public void centerOnCircle(boolean enable) { //enables PID controller.  Will overide user input to robot.
-        if (enable == false) {
-            DriverStationLCD.getInstance().println(DriverStationLCD.Line.kUser5, 1, "TARGET DISABLED");
-            turnController.disable();
-            lastEnable = false;
-        } else {
-            DriverStationLCD.getInstance().println(DriverStationLCD.Line.kUser5, 1, "TARGET ENABLED   ");
-            if (!lastEnable) {
-                lastEnable = true;
-                turnController.enable();
-                turnController.setSetpoint(gyro.pidGet());
-            }
-            lastEnable = true;
-            ColorImage image = null;
-            try {
-                if (cam.freshImage()) {// && turnController.onTarget()) {
-                    double gyroAngle = gyro.pidGet();
-                    image = cam.getImage();
-                    Thread.yield();
-                    Target[] targets = Target.findCircularTargets(image);
-                    Thread.yield();
-                    if (targets.length == 0 || targets[0].m_score < kScoreThreshold) {
-                        System.out.println("No target found");
-                        Target[] newTargets = new Target[targets.length + 1];
-                        newTargets[0] = new Target();
-                        newTargets[0].m_majorRadius = 0;
-                        newTargets[0].m_minorRadius = 0;
-                        newTargets[0].m_score = 0;
-                        for (int i = 0; i < targets.length; i++) {
-                            newTargets[i + 1] = targets[i];
-                        }
-                        //trackerDashboard.updateVisionDashboard(0.0, gyro.getAngle(), 0.0, 0.0, newTargets);
-                    } else {
-                        System.out.println(targets[0]);
-                        System.out.println("Target Angle: " + targets[0].getHorizontalAngle());
-                        turnController.setSetpoint(gyroAngle + targets[0].getHorizontalAngle());
-                        //trackerDashboard.updateVisionDashboard(0.0, gyro.getAngle(), 0.0, targets[0].m_xPos / targets[0].m_xMax, targets);
-                    }
-                }
-            } catch (NIVisionException ex) {
-                ex.printStackTrace();
-            } catch (AxisCameraException ex) {
-                ex.printStackTrace();
-            } finally {
-                try {
-                    if (image != null) {
-                        image.free();
-                    }
-                } catch (NIVisionException ex) {
-                }
-            }
-        }
-    }
+	public boolean isTargetInFrame() {
+		ColorImage image = null;
+		boolean isit = false;
+		try {
+			if (cam.freshImage()) {// && turnController.onTarget())
+				double gyroAngle = gyro.pidGet();
+				image = cam.getImage();
+				Thread.yield();
+				Target[] targets = Target.findCircularTargets(image);
+				Thread.yield();
+				if (targets.length == 0 || targets[0].m_score < kScoreThreshold) {
+				} else {
+					isit = true;
+				}
+			}
+		} catch (NIVisionException ex) {
+			ex.printStackTrace();
+			return false;
+		} catch (AxisCameraException ex) {
+			ex.printStackTrace();
+			return false;
+		} finally {
+			try {
+				if (image != null) {
+					image.free();
+				}
+			} catch (NIVisionException ex) {
+			}
+		}
+		return isit;
+	}
 
+	public boolean isTargetFound() {
+		return targetFound;
+	}
 
-    
-    public boolean isTargetInFrame(){
-        ColorImage image = null;
-        boolean isit = false;
-            try {
-                if (cam.freshImage()) {// && turnController.onTarget()) 
-                    double gyroAngle = gyro.pidGet();
-                    image = cam.getImage();
-                    Thread.yield();
-                    Target[] targets = Target.findCircularTargets(image);
-                    Thread.yield();
-                    if (targets.length == 0 || targets[0].m_score < kScoreThreshold) {
-                     
-                    } else {
-                        isit = true;
-                    }
-                }
-            } catch (NIVisionException ex) {
-                ex.printStackTrace();
-                return false;
-            } catch (AxisCameraException ex) {
-                ex.printStackTrace();
-                return false;
-            } finally {
-                try {
-                    if (image != null) {
-                        image.free();
-                    }
-                } catch (NIVisionException ex) {
-                }
-            }
-            return isit;
-    }
+	public boolean isOnTarget() {
+		if (targetFound == true) {
+			return turnController.onTarget();
+		}
+		return false;
+	}
 
-    public boolean isTargetFound(){
-        return targetFound;
-    }
+	public void setLastEnable(boolean lastEnable) {
+		this.lastEnable = lastEnable;
+	}
 
-    public boolean isOnTarget(){
-        if(targetFound == true)
-            return turnController.onTarget();
-        return false;
-    }
+	public AxisCamera getCam() {
+		return cam;
+	}
 
-    public void setLastEnable(boolean lastEnable) {
-        this.lastEnable = lastEnable;
-    }
+	public RobotDrive getDrive() {
+		return drive;
+	}
 
-    public AxisCamera getCam() {
-        return cam;
-    }
+	public Gyro getGyro() {
+		return gyro;
+	}
 
-    public RobotDrive getDrive() {
-        return drive;
-    }
+	public Joystick getJs() {
+		return js;
+	}
 
-    public Gyro getGyro() {
-        return gyro;
-    }
+	public double getkScoreThreshold() {
+		return kScoreThreshold;
+	}
 
-    public Joystick getJs() {
-        return js;
-    }
+	public boolean isLastEnable() {
+		return lastEnable;
+	}
 
-    public double getkScoreThreshold() {
-        return kScoreThreshold;
-    }
-
-    public boolean isLastEnable() {
-        return lastEnable;
-    }
-
-    public PIDController getTurnController() {
-        return turnController;
-    }
-
-
+	public PIDController getTurnController() {
+		return turnController;
+	}
 }
