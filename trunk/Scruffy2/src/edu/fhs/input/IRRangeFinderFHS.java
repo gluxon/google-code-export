5ef1f1ee5026b9815b3805485022914cce74774f
cunninghampatt@gmail.com <cunninghampatt@gmail.com@29b1f36c-e104-11dd-a550-c997509ed985>
2010-09-23 14:50:45 +0000

    Added lots of code for autonomous and implemented RobotDrive.

---
+++ trunk/Scruffy2/src/edu/fhs/input/IRRangeFinderFHS.java
@@ -0,0 +1,38 @@
+package edu.fhs.input;
+
+import edu.wpi.first.wpilibj.AnalogChannel;
+import edu.wpi.first.wpilibj.PIDSource;
+
+/**
+ * @author Team 178 Programming devision
+ */
+public class IRRangeFinderFHS extends AnalogChannel implements PIDSource
+{
+    private final double SCALE_FACTOR = 0.0; //to be changed based on results from testing the UltraSonic
+    private final double OFFSET = 0.0; //to be changed based on results from testing
+
+    public IRRangeFinderFHS(int slot, int channel)
+    {
+        super(slot, channel);
+    }
+
+    public IRRangeFinderFHS(int channel)
+    {
+        super(channel);
+    }
+
+    public double getRangeInches()
+    {
+        return getAverageVoltage() * SCALE_FACTOR - OFFSET;
+    }
+
+    public double getRangeMM()
+    {
+        return getRangeInches() * 25.4;
+    }
+
+    public double pidGet()
+    {
+        return getRangeInches();
+    }
+}
