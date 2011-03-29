3fa9a0c29e8fa8c62246957e3d4c595edc6418ed
cunninghampatt@gmail.com <cunninghampatt@gmail.com@29b1f36c-e104-11dd-a550-c997509ed985>
2011-03-29 22:24:30 +0000

    get angle on XboxControllerFHS needs to return value between 0 and 2 PI, must correct for graph quadrant

--- trunk/2011RobotProject/src/edu/fhs/input/CompressorFHS.java
+++ trunk/2011RobotProject/src/edu/fhs/input/CompressorFHS.java
@@ -19,6 +19,11 @@ public class CompressorFHS extends SensorBase
     private boolean isEnabled = false;
     private boolean isWorking = false;
 
+    /**
+     * Creates a CompressorFHS Object for operating the compressor
+     * @param relayChannel The channel input for the relay spike.
+     * @param pressureSwitchChannel The channel input for the pressure switch.
+     */
     public CompressorFHS(int relayChannel, int pressureSwitchChannel)
     { 
         this.relayChannel = relayChannel;
@@ -40,6 +45,13 @@ public class CompressorFHS extends SensorBase
         }
     }
 
+    /**
+     * Creates a CompressorFHS Object for operating the compressor
+     * @param relaySlot The slot input for the relay spike.
+     * @param relayChannel The channel input for the relay spike.
+     * @param pressureSwitchSlot The slot input for the pressure switch.
+     * @param pressureSwitchChannel The channel input for the pressure switch.
+     */
     public CompressorFHS(int relaySlot, int relayChannel, int pressureSwitchSlot, int pressureSwitchChannel)
     {
         this.relayChannel = relayChannel;
@@ -61,21 +73,34 @@ public class CompressorFHS extends SensorBase
         }
     }
 
+    /**
+     * Starts the compressor
+     */
     public void start()
     {
         isEnabled = true;
     }
 
+    /**
+     * Stops the compressor
+     */
     public void stop()
     {
         isEnabled = false;
     }
 
+    /**
+     * Gets the current state of the compressor
+     * @return The current state of the compressor.
+     */
     public boolean getState()
     {
         return isWorking;
     }
 
+    /**
+     * Checks the pressure switch and the isEnabled input to turn the compressor on or off.
+     */
     private class CompressorFHSThread extends Thread
     {
         public void run()
