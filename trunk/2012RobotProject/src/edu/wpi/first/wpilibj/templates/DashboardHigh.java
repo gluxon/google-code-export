352169689f10ac40e8d8e40add880803d44a9f88
cunninghampatt@gmail.com <cunninghampatt@gmail.com@29b1f36c-e104-11dd-a550-c997509ed985>
2012-02-17 21:42:18 +0000

    sensor changes

--- trunk/2012RobotProject/src/edu/wpi/first/wpilibj/templates/DashboardHigh.java
+++ trunk/2012RobotProject/src/edu/wpi/first/wpilibj/templates/DashboardHigh.java
@@ -13,7 +13,7 @@ public class DashboardHigh
 
     }
 
-    public void updateDashboardHigh(Drivetrain drivetrain, double gyro, double ultrasonic, double ultrasonic2, double pressure, double numParticles, Joystick joystick)
+    public void updateDashboardHigh(Drivetrain drivetrain, double gyro, double ultrasonic, double ultrasonic2, double numParticles, boolean compressorState, Joystick joystick)
     {
         // [frontLeftVictor],[frontRightVictor],[rearLeftVictor],[rearRightVictor],[Gyro],[Ultrasonic],[Joystick X],[Joystick Y],[Joystick Z]
         dashboardHigh = DriverStation.getInstance().getDashboardPackerHigh();
@@ -26,14 +26,15 @@ public class DashboardHigh
         dashboardHigh.addDouble(drivetrain.getRearRight());
         dashboardHigh.addDouble(gyro);
         dashboardHigh.addDouble(ultrasonic);
-		dashboardHigh.addDouble(ultrasonic2);
+	dashboardHigh.addDouble(ultrasonic2);
         dashboardHigh.addDouble(joystick.getX());
         dashboardHigh.addDouble(joystick.getY());
         dashboardHigh.addDouble(joystick.getZ());
-		dashboardHigh.addDouble(pressure);
-		dashboardHigh.addDouble(numParticles);
+	dashboardHigh.addDouble(0.0);//remove pressure
+	dashboardHigh.addDouble(numParticles);
+        //dashboardHigh.addBoolean(compressorState);
         dashboardHigh.finalizeCluster();
         dashboardHigh.finalizeCluster();
-		dashboardHigh.commit();
+	dashboardHigh.commit();
     }
 }
